dd87fe7636b54509ccb3dda0f8f09b70977a4b9e
chloHabitto <chloe@habitto.nl>
2026-01-13 15:12:11 +0100

    Initial Commit

---
+++ LastStraw/LastStrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastStrawApp.swift
+//  LastStraw
+//
+//  Created by Chloe Lee on 2026-01-13.
+//
+
+import SwiftUI
+
+@main
+struct LastStrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LastStrawTests/LastStrawTests.swift
@@ -0,0 +1,16 @@
+//
+//  LastStrawTests.swift
+//  LastStrawTests
+//
+//  Created by Chloe Lee on 2026-01-13.
+//
+
+import Testing
+
+struct LastStrawTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
